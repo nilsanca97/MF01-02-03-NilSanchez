026bb00115bd1e35cec1bd96a1d931004a9e7dec
nil <nilsanca97@gmail.com>
2025-11-03 22:31:15 +0100

    modify to RentingCarBootApplication to test method populateInssuranceCiaData. connection with db works correct but the method populateInssuranceCiaData don't create data to table InsuanceCia_Delegation

--- rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/RentingCarBootApplication.java
+++ rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/RentingCarBootApplication.java
@@ -1,9 +1,9 @@
 package dev.app.rentingCar_boot;
 
-import dev.app.rentingCar_boot.utils.PopulateInssuranceCia;
+//import dev.app.rentingCar_boot.utils.PopulateInssuranceCia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+//import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class RentingCarBootApplication {
