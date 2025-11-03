ffe13d225e567de46cf81ca18501502de52258ca
nil <nilsanca97@gmail.com>
2025-11-03 20:19:25 +0100

    change 2 relationship between inssuranceCia & Car. Now is @ManyToMany. The connection with DB works succesfully. Now only 4 message alerts (!)(instead of 6)

--- rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/model/InssuranceCia.java
+++ rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/model/InssuranceCia.java
@@ -5,9 +5,7 @@ import dev.app.rentingCar_boot.utils.GenerateUUID;
 import jakarta.persistence.*;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 public class InssuranceCia {
