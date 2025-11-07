2bb8279ea70a8e323f4f4b92431e336fb564c4b8
nil <nilsanca97@gmail.com>
2025-11-07 12:11:56 +0100

    CarRepository(interface): 4.2 ensure repositories support CRUD operations for managing the relationships

--- rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/CarRepository.java
+++ rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/CarRepository.java
@@ -5,7 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CarRepository extends JpaRepository<Car, String> {
+    Iterable<Car> findAll();
+    Optional<Car> findCarById(String id);
+    List<Car> findByYear (int year);
     List<Car> findByBrand(String brand);
+
+    Optional<Car> deleteCarById (String id);
+    Optional<Car> updateCar (Car car);
 }
