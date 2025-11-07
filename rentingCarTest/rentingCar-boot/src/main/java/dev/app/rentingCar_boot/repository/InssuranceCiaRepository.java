56e9a5e5b6da0e7277310f23f8f74a8937bc51a2
nil <nilsanca97@gmail.com>
2025-11-07 11:34:35 +0100

    InssuranceCiaRepository(interface): Create JPA repository interface for InssuraceCia

--- rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/InssuranceCiaRepository.java
+++ rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/InssuranceCiaRepository.java
@@ -1,7 +1,8 @@
 package dev.app.rentingCar_boot.repository;
 
 import dev.app.rentingCar_boot.model.InssuranceCia;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InssuranceCiaRepository extends CrudRepository<InssuranceCia, String> {
+public interface InssuranceCiaRepository extends JpaRepository<InssuranceCia, String> {
+
 }
