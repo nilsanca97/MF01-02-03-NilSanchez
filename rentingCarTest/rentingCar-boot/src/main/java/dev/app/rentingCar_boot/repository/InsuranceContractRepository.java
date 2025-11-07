a20a652c418f0f469c2ffb63f6114cee1f10202b
nil <nilsanca97@gmail.com>
2025-11-07 12:11:27 +0100

    InsuranceContractRepository(interface): 4.2 ensure repositories support CRUD operations for managing the relationships

--- rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/InsuranceContractRepository.java
+++ rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/InsuranceContractRepository.java
@@ -3,6 +3,13 @@ package dev.app.rentingCar_boot.repository;
 import dev.app.rentingCar_boot.model.InsuranceContract;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InsuranceContractRepository extends JpaRepository<InsuranceContract, String> {
 
+    // Search contracts by CarId
+    List<InsuranceContract> findByCar_Id(String carId);
+
+    // Search contracts by InssuranceCiaId
+    List<InsuranceContract> findByInssuranceCia_Id(String InssuranceCiaId);
 }
