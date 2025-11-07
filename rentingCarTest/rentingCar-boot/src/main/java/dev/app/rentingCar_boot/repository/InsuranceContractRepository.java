ccd7e1b4dfa30eadcba9a6967213afcadc06a4d0
nil <nilsanca97@gmail.com>
2025-11-07 12:30:59 +0100

    InsuranceContract(model): modify relation @ManyToOne between InsuranceContract-Car (delete cascade = cascadeType.ALL). & modify relation @ManyToOne (between InsuranceContract-InssuranceCia): delete cascade

--- rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/InsuranceContractRepository.java
+++ rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/InsuranceContractRepository.java
@@ -1,14 +1,21 @@
 package dev.app.rentingCar_boot.repository;
 
+import dev.app.rentingCar_boot.model.Car;
+import dev.app.rentingCar_boot.model.InssuranceCia;
 import dev.app.rentingCar_boot.model.InsuranceContract;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface InsuranceContractRepository extends JpaRepository<InsuranceContract, String> {
+    // search Insurance contracts by Car
+    List<InsuranceContract> findByCar(Car car);
 
     // Search contracts by CarId
     List<InsuranceContract> findByCar_Id(String carId);
+
+    // Search contracts by Inssurance Companies
+    List<InsuranceContract> findByInssuranceCia(InssuranceCia inssuranceCia);
 
     // Search contracts by InssuranceCiaId
     List<InsuranceContract> findByInssuranceCia_Id(String InssuranceCiaId);
