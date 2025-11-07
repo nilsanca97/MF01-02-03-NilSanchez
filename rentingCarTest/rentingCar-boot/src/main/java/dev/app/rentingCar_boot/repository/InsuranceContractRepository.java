9b424081502dfa02bd24ff62a2f183831f2fbebe
nil <nilsanca97@gmail.com>
2025-11-07 11:36:31 +0100

    InsuranceContractRepository(interface): Create JPA repository interface for InsuranceContract

--- rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/InsuranceContractRepository.java
+++ rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/InsuranceContractRepository.java
@@ -2,7 +2,6 @@ package dev.app.rentingCar_boot.repository;
 
 import dev.app.rentingCar_boot.model.InsuranceContract;
 import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.repository.CrudRepository;
 
 public interface InsuranceContractRepository extends JpaRepository<InsuranceContract, String> {
 
