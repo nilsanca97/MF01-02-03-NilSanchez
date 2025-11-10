3c71fbea614c2044cb8cb12b95628e80c9d4f5a4
nil <nilsanca97@gmail.com>
2025-11-10 18:01:36 +0100

    modify CarRepository (interface). Comment l19 updateCar

--- rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/CarRepository.java
+++ rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/CarRepository.java
@@ -2,17 +2,19 @@ package dev.app.rentingCar_boot.repository;
 
 import dev.app.rentingCar_boot.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car, String> {
-    Iterable<Car> findAll();
+    //Optional <Car> findAll();
     Optional<Car> findCarById(String id);
     List<Car> findByYear (int year);
     List<Car> findByBrand(String brand);
 
     Optional<Car> deleteCarById (String id);
-    Optional<Car> updateCar (Car car);
+    //Optional<Car> updateCar (Car car);
 }
