417a828d4610fc151865c270ddde13d0cb7efcef
nil <nilsanca97@gmail.com>
2025-11-11 18:44:24 +0100

    create utility class "RandomDates2025", for help and use in PopulateInsuranceContract

---
+++ rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/utils/RandomDates2025.java
@@ -0,0 +1,54 @@
+package dev.app.rentingCar_boot.utils;
+
+import java.time.LocalDate;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomDates2025 {
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    // Constructor vacío
+    public RandomDates2025() {}
+
+    // Constructor que genera fechas aleatorias dentro de 2025
+    public void generateRandomDates2025() {
+        LocalDate startOfYear = LocalDate.of(2025, 1, 1);
+        LocalDate endOfYear = LocalDate.of(2025, 12, 31);
+
+        long minDay = startOfYear.toEpochDay();
+        long maxDay = endOfYear.toEpochDay();
+
+        long randomStartDay = ThreadLocalRandom.current().nextLong(minDay, maxDay + 1);
+        this.startDate = LocalDate.ofEpochDay(randomStartDay);
+
+        long randomEndDay = ThreadLocalRandom.current().nextLong(randomStartDay, maxDay + 1);
+            this.endDate = LocalDate.ofEpochDay(randomEndDay);
+    }
+
+    // Getters y Setters
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "ContractDate {" +
+               "startDate=" + startDate +
+               ", endDate=" + endDate +
+              '}';
+    }
+}
