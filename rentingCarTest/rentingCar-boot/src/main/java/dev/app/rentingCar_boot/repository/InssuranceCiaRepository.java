76c3180a5736df3abe6e7c34669b072e493118eb
nil <nilsanca97@gmail.com>
2025-11-12 18:14:38 +0100

    add findById to InssuranceCiaRepository (interface)

--- rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/InssuranceCiaRepository.java
+++ rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/InssuranceCiaRepository.java
@@ -3,6 +3,9 @@ package dev.app.rentingCar_boot.repository;
 import dev.app.rentingCar_boot.model.InssuranceCia;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InssuranceCiaRepository extends JpaRepository<InssuranceCia, String> {
+import java.util.Optional;
 
+public interface InssuranceCiaRepository extends JpaRepository<InssuranceCia, String> {
+    //@Override
+    Optional<InssuranceCia> findById(String id);
 }
