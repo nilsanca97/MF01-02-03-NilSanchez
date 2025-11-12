e698635302cbef938d33db413649d454c97e41d4
nil <nilsanca97@gmail.com>
2025-11-12 17:47:57 +0100

    Test: modify PopulateTests: add 6. populateInsuranceContract. db conection works correctly

--- rentingCarTest/rentingCar-boot/src/test/java/dev/app/rentingCar_boot/PopulateTests.java
+++ rentingCarTest/rentingCar-boot/src/test/java/dev/app/rentingCar_boot/PopulateTests.java
@@ -24,35 +24,43 @@ public class PopulateTests {
     PopulateInssuranceCia populateInssuranceCia;
 
     @Autowired
+    PopulateInsuranceContract populateInsuranceContract;
+    @Autowired
     PopulateAllTables populateAllTables;
 
     @Test
     void populateAllTables () {
 
-        // let s populate cars first
+        // 1.let s populate cars first
         PopulateStatus populateCarStatus = populateCar.populateCar(10);
         System.out.println("\nPopulate Car operations: " + populateCarStatus.getQty() +
                 " \n" + populateCarStatus.getMessage());
 
-        // lets populate Inssurance companies
+        // 2.lets populate Inssurance companies
         PopulateStatus populateInssuranceCiaStatus= populateInssuranceCia.populateInssuranceCia(10);
         System.out.println("\n Populate InssuranceCia operations: "+ populateInssuranceCiaStatus.getQty() +
                 " \n" + populateInssuranceCiaStatus.getMessage());
 
-        // let s populate clients
+        // 3.let s populate clients
         PopulateStatus populateClientStatus = populateClient.populateClient(10);
         System.out.println("\nPopulate Client operations: " + populateClientStatus.getQty() +
                 " \n" + populateClientStatus.getMessage());
 
-        // once cars are populated, let s populate bookings
+        // 4.once cars are populated, let s populate bookings
         PopulateStatus populateBookingStatus = populateBooking.populateBooking(10);
         System.out.println("\nPopulate Booking operations: " + populateBookingStatus.getQty() +
                 " \n" + populateBookingStatus.getMessage());
 
-        // once bookings are populated, let s populate driving courses
+        // 5.once bookings are populated, let s populate driving courses
         PopulateStatus populateDrivingCourseStatus = populateDrivingCourse.populateDrivingCourse(10);
         System.out.println("\nPopulate DrivingCourse operations: " + populateDrivingCourseStatus.getQty() +
                 " \n" + populateDrivingCourseStatus.getMessage());
+
+        // 6.lets populate Inssurance companies
+        PopulateStatus populateInsuranceContractStatus = populateInsuranceContract.populateInsuranceContract(10);
+        System.out.println("\n Populate InsuranceContract operations: "+ populateInsuranceContractStatus.getQty() +
+                " \n" + populateInsuranceContractStatus.getMessage());
+
     }
 
     @Test
