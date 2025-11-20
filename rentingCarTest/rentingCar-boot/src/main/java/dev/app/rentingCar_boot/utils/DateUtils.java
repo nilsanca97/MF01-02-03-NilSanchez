a8a7213953f1bd17cb8a54e84208932cb555e979
nil <nilsanca97@gmail.com>
2025-11-20 19:50:29 +0100

    modify DateUtils to solve error in InsuranceContractTest (to populate InsuranceContract). Error with format date dd/MM/Year

--- rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/utils/DateUtils.java
+++ rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/utils/DateUtils.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
 
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public static LocalDate parseDate(String dateString) {
         return LocalDate.parse(dateString, FORMATTER);
