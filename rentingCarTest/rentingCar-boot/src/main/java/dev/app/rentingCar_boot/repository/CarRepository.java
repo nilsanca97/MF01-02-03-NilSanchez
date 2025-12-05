edd944cc84539ee8a3fada88ee2aa998c5a81004
nil <nilsanca97@gmail.com>
2025-12-05 08:40:33 +0100

    update docs

--- rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/CarRepository.java
+++ rentingCarTest/rentingCar-boot/src/main/java/dev/app/rentingCar_boot/repository/CarRepository.java
@@ -4,7 +4,6 @@ import dev.app.rentingCar_boot.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 import java.util.List;
 import java.util.Optional;
 
